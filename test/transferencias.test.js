c13f1e79a46dd5822693212d8141c7a06fcbf797
Adriana Lima <90102794+Adrianakellysl@users.noreply.github.com>
2026-04-01 11:27:52 -0300

    Adicionei testes para GET transferencias total e individual

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -5,13 +5,13 @@ const { obterToken } = require('../helpers/autenticacao')
 const postTransferencia = require('../fixtures/postTransferencia.json')
 
 describe('Transferências', () => {
-  describe('POST / transferencias', () => {
-    let token
+  let token
     
     beforeEach(async () => {
       token = await obterToken('adriana.lima', '123456')
     })
 
+  describe('POST / transferencias', () => {
     it('Deve retornar sucesso com 201 quando o valor foi igual ou maior que R$10,00', async () => {
       const bodyTransferencias = { ...postTransferencia }
 
@@ -38,6 +38,32 @@ describe('Transferências', () => {
       
       expect(resposta.status).to.equal(422);
       console.log(resposta.body)  
+    })
+  })
+
+  describe('GET / transferencias/{id}', () => { 
+    it('Deve retornar sucesso com 200 e dados iguais ao registro de transferências  do banco de dados quando o ID for válido', async () => {
+      const resposta = await request(process.env.BASE_URL)
+        .get('/transferencias/48')
+        .set('Authorization', `Bearer ${token}`)
+
+        expect(resposta.status).to.equal(200)
+        expect(resposta.body.id).to.equal(48)
+        expect(resposta.body.id).to.be.a('number')
+        expect(resposta.body.conta_origem_id).to.equal(2)
+    })
+  })
+  
+
+   describe('GET / transferencias/{id}', () => { 
+    it('Deve retornar 10 elementos na paginação quando informar o limite de 10 registros', async () => { 
+      const resposta = await request(process.env.BASE_URL)
+        .get('/transferencias?page=1&limit=10')
+        .set('Authorization', `Bearer ${token}`)
+
+      expect(resposta.status).to.equal(200)
+      expect(resposta.body.limit).to.equal(10)
+      expect(resposta.body.transferencias).to.have.lengthOf(10)
     })
   })
 })
